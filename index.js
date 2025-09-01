a90d6d6f8f41f851a23c5ab3549effe3a638db38
GriffithS17 <griffithsulista11@gmail.com>
2025-09-01 15:34:55 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function mudarb() {
+  document.getElementById("link").style.setProperty("color", "#000", "important")
+  document.querySelector("#link span").style.setProperty("color", "blueviolet", "important")
+}
+
+function mudarw() {
+  document.getElementById("link").style.color = "#fff"
+  document.getElementById("spanText").style.color =""
+}
